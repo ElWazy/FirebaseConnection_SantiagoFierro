1c4febf7175954f3251b2368f74402ca91ab08f3
ElWazy <ElCazero.Fer@gmail.com>
2021-11-29 10:18:44 -0300

    Load Components

--- app/src/main/java/com/lutrias/firebaseconnection_santiagofierro/ListActivity.java
+++ app/src/main/java/com/lutrias/firebaseconnection_santiagofierro/ListActivity.java
@@ -3,12 +3,35 @@ package com.lutrias.firebaseconnection_santiagofierro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private Button btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        loadComponents();
+        assignButtons();
+    }
+
+    private void loadComponents() {
+        listView = findViewById(R.id.listView);
+
+        btnDelete = findViewById(R.id.btnDelete);
+    }
+
+    private void assignButtons() {
+        btnDelete.setOnClickListener(this::delete);
+    }
+
+    private void delete(View view) {
+
     }
 }
--- app/src/main/java/com/lutrias/firebaseconnection_santiagofierro/MainActivity.java
+++ app/src/main/java/com/lutrias/firebaseconnection_santiagofierro/MainActivity.java
@@ -3,12 +3,43 @@ package com.lutrias.firebaseconnection_santiagofierro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtName, txtSalary, txtProfession;
+    private Button btnSave, btnGoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadComponents();
+        assignButtons();
+    }
+
+    private void loadComponents() {
+        txtName = findViewById(R.id.txtName);
+        txtSalary = findViewById(R.id.txtSalary);
+        txtProfession = findViewById(R.id.txtProfession);
+
+        btnSave = findViewById(R.id.btnSave);
+        btnGoList = findViewById(R.id.btnGoList);
+    }
+
+    private void assignButtons() {
+        btnSave.setOnClickListener(this::save);
+        btnGoList.setOnClickListener(this::goList);
+    }
+
+    private void save(View view) {
+
+    }
+
+    private void goList(View view) {
+
     }
 }
