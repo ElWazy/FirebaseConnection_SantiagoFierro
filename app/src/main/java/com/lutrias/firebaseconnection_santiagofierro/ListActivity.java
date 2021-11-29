8ab19bec448a2495bebfe717116462e20f0280d8
ElWazy <ElCazero.Fer@gmail.com>
2021-11-29 12:30:54 -0300

    Remove Professional From List

--- app/src/main/java/com/lutrias/firebaseconnection_santiagofierro/ListActivity.java
+++ app/src/main/java/com/lutrias/firebaseconnection_santiagofierro/ListActivity.java
@@ -6,9 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.ChildEventListener;
@@ -31,6 +33,7 @@ public class ListActivity extends AppCompatActivity {
     private DatabaseReference databaseReference;
 
     private List<Professional> professionals;
+    private Professional selected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +44,32 @@ public class ListActivity extends AppCompatActivity {
 
         loadComponents();
         assignButtons();
+        fillListView();
+    }
 
+    private void initDatabase() {
+        FirebaseApp.initializeApp(this);
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference();
+    }
+
+    private void loadComponents() {
+        listView = findViewById(R.id.listView);
+        btnDelete = findViewById(R.id.btnDelete);
+    }
+
+    private void assignButtons() {
+        listView.setOnItemClickListener(this::selectProfessional);
+        btnDelete.setOnClickListener(this::delete);
+    }
+
+    private void fillListView() {
         professionals = new ArrayList<>();
         databaseReference.child("Professional").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                professionals.clear();
+
                 for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
                     Professional professional = dataSnapshot.getValue(Professional.class);
 
@@ -67,23 +91,20 @@ public class ListActivity extends AppCompatActivity {
         });
     }
 
-    private void initDatabase() {
-        FirebaseApp.initializeApp(this);
-        database = FirebaseDatabase.getInstance();
-        databaseReference = database.getReference();
-    }
-
-    private void loadComponents() {
-        listView = findViewById(R.id.listView);
-
-        btnDelete = findViewById(R.id.btnDelete);
-    }
-
-    private void assignButtons() {
-        btnDelete.setOnClickListener(this::delete);
+    private void selectProfessional(AdapterView<?> adapterView, View view, int i, long l) {
+        selected = (Professional) adapterView.getItemAtPosition(i);
     }
 
     private void delete(View view) {
+        Professional professional = new Professional();
 
+        professional.setId(selected.getId());
+
+        databaseReference
+                .child("Professional")
+                .child(professional.getId())
+                .removeValue();
+
+        Toast.makeText(getBaseContext(), "Has eliminado un profesional", Toast.LENGTH_SHORT).show();
     }
 }
